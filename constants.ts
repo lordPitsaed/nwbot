262b4e306dd7f65cee97475226a6336904524497
Dmitriy <usakovd00@gmail.com>
2024-03-18 21:13:22 +0300

    fixed folder error

--- constants.ts
+++ constants.ts
@@ -1,7 +1,8 @@
 import dotenv from 'dotenv';
 import fs from 'fs';
 dotenv.config();
-export const userIds = (() => fs.readFileSync('./remembered_user_ids.txt').toString().split('\n'))();
+export const userIds = (() =>
+  fs.readFileSync('./remembered_user_ids/remembered_user_ids.txt').toString().split('\n'))();
 
 export const CHAT_ID = process.env.CHAT_ID || '';
 export const ALERT_TOPIC = process.env.BOT_NOTIFICATIONS_TOPIC || '';
