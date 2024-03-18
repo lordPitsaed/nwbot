cbb6ec40215f8fe462fd24d6796f1044ebfd6a7d
Dmitriy <usakovd00@gmail.com>
2024-03-18 21:24:12 +0300

    avoiding multiple rememberences

--- modules/user_remembering_module.ts
+++ modules/user_remembering_module.ts
@@ -6,9 +6,13 @@ import { bot } from '../main';
 export const userRememberingModule = () => {
   bot.onText(/\/rememberme/, (msg) => {
     if (msg.chat.type === 'private') {
-      fs.appendFileSync('./data_files/remembered_user_ids.txt', String(msg.from?.id) + '\n');
-      bot.sendMessage(msg.chat.id, `Q ${msg.from?.username}, ur id is: ${msg.from?.id}. I'll remember you`);
-      getUserNamesById(bot, userIds, CHAT_ID);
+      if (!fs.readFileSync('./data-files/remembered_user_ids.txt').toString().includes(String(msg.from?.id))) {
+        fs.appendFileSync('./data_files/remembered_user_ids.txt', String(msg.from?.id) + '\n');
+        bot.sendMessage(msg.chat.id, `Q ${msg.from?.username}, ur id is: ${msg.from?.id}. I'll remember you`);
+        getUserNamesById(bot, userIds, CHAT_ID);
+      } else {
+        bot.sendMessage(msg.chat.id, 'I can remember you already, ' + msg.from?.username);
+      }
     } else {
       bot.sendMessage(msg.chat.id, `Try to use this in DM.`);
     }
