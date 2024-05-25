04839bdf9e40bafa54237b9e64ea80ecb2f159b2
Lord Pitsaed <LordPitsaed@some.com>
2024-05-25 12:45:27 +0300

    saving keyboard message to update it

--- modules/keyobard_module.ts
+++ modules/keyobard_module.ts
@@ -1,9 +1,21 @@
+import TelegramBot from "node-telegram-bot-api";
 import { bot } from "../main";
+import fs from "fs";
+
+const KEYBOARD_MESSAGE = "./data_files/keyboard_message.json";
 
 export const keyboardModule = () => {
-  bot.on("text", (msg) => {
+  let message: TelegramBot.Message = JSON.parse(
+    fs.readFileSync(KEYBOARD_MESSAGE, {
+      encoding: "utf-8",
+    })
+  );
+  bot.on("text", async (msg) => {
     if (msg.text?.toLowerCase() === "бот ку") {
-      bot.sendMessage(msg.chat.id, "ку", {
+      if (message.hasOwnProperty("chat") && message.chat.id === msg.chat.id) {
+        bot.deleteMessage(message.chat.id, message.message_id);
+      }
+      const options = {
         reply_markup: {
           keyboard: [
             [{ text: "PRAY FOR NWB" }],
@@ -14,8 +26,18 @@ export const keyboardModule = () => {
           one_time_keyboard: true,
         },
         message_thread_id: msg.message_thread_id,
-        reply_to_message_id: msg.message_id,
-      });
+      };
+
+      try {
+        message = await bot.sendMessage(msg.chat.id, "ку", {
+          ...options,
+          reply_to_message_id: msg.message_id,
+        });
+      } catch {
+        message = await bot.sendMessage(msg.chat.id, "ку", options);
+      }
+
+      fs.writeFileSync(KEYBOARD_MESSAGE, JSON.stringify(message));
     }
   });
 };
