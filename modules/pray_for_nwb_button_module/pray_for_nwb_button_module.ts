04bdb6c05f8ab89804ae89b5ca09c1d680398b02
Lord Pitsaed <lordpitsaed@example.com>
2025-06-03 17:56:48 +0300

    added shadow tax bible(rofls) + new quotes

--- modules/pray_for_nwb_button_module/pray_for_nwb_button_module.ts
+++ modules/pray_for_nwb_button_module/pray_for_nwb_button_module.ts
@@ -1,8 +1,10 @@
+import schedule from "node-schedule";
 import { getRandInt } from "../../helpers";
 import { startTimeout } from "../../helpers/timers";
 import { bot } from "../../main";
 
-import { quotes } from "./quotes";
+import { envVars } from "../../constants";
+import { quotes, shadowTaxBible } from "./quotes";
 
 export const prayForNwbButtonModule = () => {
   let getTimeRemaining = () => 0;
@@ -17,7 +19,7 @@ export const prayForNwbButtonModule = () => {
         const randomQuote = quotes[randomIndex || Math.floor(Math.random() * (quotes.length - 1))];
 
         bot.sendMessage(msg.chat.id, randomQuote, { reply_to_message_id: msg?.message_id }).then();
-        getTimeRemaining = startTimeout(3600000).getTimeRemaining as () => number;
+        getTimeRemaining = startTimeout(1800000).getTimeRemaining as () => number;
       } else {
         bot
           .sendMessage(
@@ -33,3 +35,14 @@ export const prayForNwbButtonModule = () => {
     }
   });
 };
+
+const pbRule = new schedule.RecurrenceRule();
+pbRule.hour = 6;
+pbRule.minute = 0;
+pbRule.tz = "Europe/Moscow";
+schedule.scheduleJob(pbRule, async () => {
+  const randomIndex = await getRandInt(0, quotes.length);
+  const randomQuote = shadowTaxBible[randomIndex || Math.floor(Math.random() * (shadowTaxBible.length - 1))];
+
+  bot.sendMessage(envVars.CHAT_ID, `${randomQuote} Аминь. НВБ покоряет все без помех и препятствий!`);
+});
